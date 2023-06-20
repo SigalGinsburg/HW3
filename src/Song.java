7780c0c90b1fb96cf98608ae7a3f44ac988c6568
mayasokolov <mayasokolov@campus.technion.ac.il>
2023-06-20 23:13:28 +0300

    doneeeeeeeeeeeeee

--- src/Song.java
+++ src/Song.java
@@ -27,7 +27,7 @@ public class Song implements Cloneable {
         this.durationInSec = durationInSec;
     }
 
-    /** this function implements a deep copy of a song **/
+    /** deep copy of a song, as we learned to implement in the course **/
     @Override
     protected Song clone()  {
         try {
@@ -44,7 +44,7 @@ public class Song implements Cloneable {
     @Override
     public String toString() {
         StringBuilder songString = new StringBuilder();
-        songString.append(this.name + ", " + this.artist + ", " + this.genre + ", ");
+        songString.append(this.name + ", " + this.artist + ", " + this.genre + ", "); //creating the string as directed
         songString.append(this.durationToString()); //help function that creates a string for the duration of the song
         String result= songString.toString();
         return result;
@@ -52,13 +52,14 @@ public class Song implements Cloneable {
 
     /** An helper function for 'toString' function. **/
     private String durationToString() {
-        int howManyMin = this.durationInSec / 60;
-        int howManySec = this.durationInSec % 60;
+        // changing the value to min:sec display
+        int howManyMin = this.durationInSec / 60; // how many minutes
+        int howManySec = this.durationInSec % 60; // how many seconds
         StringBuilder durationString = new StringBuilder();
         durationString.append(howManyMin);
         durationString.append(":");
         if (howManySec <= 9) {
-            durationString.append("0");
+            durationString.append("0"); //adding zero if necessary
             durationString.append(howManySec);
 
         } else {
@@ -68,6 +69,11 @@ public class Song implements Cloneable {
         return result;
     }
 
+    /**
+     * checks whether two songs are equals i.e. same song's name and the same artist.
+     * @param songToComper the song that is compared to 'this' song
+     * @return boolean answer
+     */
     @Override
     public boolean equals(Object songToComper) {
         if (songToComper instanceof Song) {
